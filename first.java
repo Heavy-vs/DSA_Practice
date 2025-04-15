1b1cda0be142273c4e30b2fb6f3ca492a2f27dd4
Vaibhav Sharma <vs8595257872@gmail.com>
2025-04-15 23:57:21 +0530

    New

---
+++ first.java
@@ -0,0 +1,12 @@
+package DSA_practice;
+
+public class first{
+    public static void main(String[] args) {
+        int[] arr ={5,2,7,9,6};
+        int x=0;
+        for (int i= 0; i <arr.length; i++) {
+            x= x+arr[i];
+        }
+        System.out.println(x);
+    }
+}
